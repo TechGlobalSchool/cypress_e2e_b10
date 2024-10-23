228e230b184e1eb0f76825cd900b980e1bc3e5f7
TechGlobalSchool <tgschool01@gmail.com>
2024-10-22 21:13:34 -0500

    Locators

--- cypress/e2e/integration/02-locators.cy.js
+++ cypress/e2e/integration/02-locators.cy.js
@@ -1,7 +1,49 @@
 /// <reference types="cypress"/>
 
-// This is describe test block that holds group of tests
-describe('CSS Locators', () => {
+describe("CSS Locators", () => {
+  it("Understanding CSS Syntax - Locating using tags", () => {
+    cy.visit("https://www.techglobal-training.com/frontend/html-elements");
 
-  
-})
+    cy.get("button");
+
+    cy.get("h3");
+
+    cy.get("li");
+
+    cy.get("input");
+  });
+
+  it("Understanding CSS Syntax - Locating class and ID", () => {
+    cy.visit("https://www.techglobal-training.com/frontend/html-elements");
+
+
+    cy.get('#checkbox-button-group')
+
+    cy.get('.checkbox')
+
+    // class="nadja_tugba youseff jibril kareem maria alina david timur bermet"
+  });
+
+  it("Understanding CSS Syntax - Locating web elements using multiple selectors", () => {
+    cy.visit("https://www.techglobal-training.com/frontend/html-elements");
+
+    
+    cy.get('label.checkbox.is-inline')
+  });
+
+  it("Understanding CSS Syntax - Locating child, descendant, adjacent web element", () => {
+    cy.visit("https://www.techglobal-training.com/frontend/html-elements");
+
+    /**
+     * Child Selector ( > ) *
+     * 
+     * Description: Targets direct children of a specified parent element.
+     */
+
+    cy.get('#checkbox-button-group > h3')
+
+    cy.get('#checkbox-button-group > div > #apple_check > #checkbox_1')
+
+    cy.get('#checkbox-button-group > div > .checkbox > #checkbox_1.mr-2')
+  });
+});
