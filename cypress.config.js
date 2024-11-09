23c34f7de796d8d34a7d70cdef2e39acb05192c8
TechGlobalSchool <tgschool01@gmail.com>
2024-11-09 11:15:32 -0600

    Reports

--- cypress.config.js
+++ cypress.config.js
@@ -13,10 +13,11 @@ module.exports = defineConfig({
     UI_USERNAME: process.env.UI_USERNAME,
     UI_PASSWORD: process.env.UI_PASSWORD,
   },
-
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'https://www.techglobal-training.com'
   },
